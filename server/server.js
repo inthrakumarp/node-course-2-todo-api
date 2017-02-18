df0510c1d5896e10488ee79b9523086a013c3686
inthrakumar <inthrakumar@gmail.com>
2017-02-18 15:17:50 +0530

    todo express Rest api

---
+++ server/server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    // res.send(req.body);
+    // next();
+    var newTodo = new Todo({
+        text: req.body.text
+    });
+    
+    newTodo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        console.log("Unable to create Todo");
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server has started listening @ :3000");
+})
+// var newTodo = new Todo({
+//     text: "Learn Jquery",
+//     completed: true,
+//     completedAt: 15-05-2016
+// });
+
+// var newUser = new User({
+//    email: '    inthra    '
+// });
+
+// newTodo.save().then((doc) =>{
+//     console.log(doc);    
+// }, (e) => {
+//     console.log("Unable to connect");
+// })
+
+// newUser.save().then((doc) => {
+//     console.log(doc);
+// }, (e) => {
+//     console.log("Unable to create user", e);
+// })
