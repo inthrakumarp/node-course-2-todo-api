adb34aa88c23da3d9cf278030cf8f32b48f9a41f
inthrakumar <inthrakumar@gmail.com>
2017-02-23 03:38:48 +0530

    Get todos :id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,24 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            res.status(404).send();
+        }
+        
+        res.send({todo});
     }, (e) => {
         res.status(400).send(e);
     })
